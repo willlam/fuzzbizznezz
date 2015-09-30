0672628d070b164e4c87d89356f78e3356b73b9a
will <will@willlam.net>
2015-09-30 13:22:16 -0400

    Initial Commit

---
+++ FuzzBizzNezz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FuzzBizzNezz
+//
+//  Created by William Lam on 2015-09-30.
+//  Copyright © 2015 William Lam. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
