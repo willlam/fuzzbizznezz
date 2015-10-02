644f1dcb7a640c483e1ed3b00c0c650abc26620d
will <will@willlam.net>
2015-10-02 17:06:57 -0400

    split out fuzzbizz portion into its own function

--- FuzzBizzNezz/main.c
+++ FuzzBizzNezz/main.c
@@ -8,7 +8,23 @@
 
 #include <stdio.h>
 
+void fuzzBizzNezz(int fuzz, int bizz, int nezz) {
+  for (int i = 1; i <= nezz; i++) {
+    if (i % fuzz == 0 && i % bizz == 0) {
+      printf("FuzzBizz\n");
+    } else if (i % fuzz == 0) {
+      printf("Fuzz\n");
+    } else if (i % bizz== 0) {
+      printf("Bizz\n");
+    } else {
+      printf("%d\n", i);
+    }
+  }
+}
+
+
 int main() {
+  
   int fuzz, bizz, nezz;
   printf("Please input three integers for 'Fuzz', 'Bizz' and 'Nezz': \n");
   scanf("%d %d %d", &fuzz, &bizz, &nezz);
@@ -16,19 +32,8 @@ int main() {
   printf("Integer for 'Bizz' is %d\n", bizz);
   printf("Integer for 'Nezz' is %d\n", nezz);
   
-  
-  
-  for (int i = 1; i <= nezz; i++) {
-    if (i%fuzz==0 && i%bizz==0) {
-      printf("FuzzBizz\n");
-    } else if (i%fuzz==0) {
-      printf("Fuzz\n");
-    } else if (i%bizz==0) {
-      printf("Bizz\n");
-    } else {
-      printf("%d\n", i);
-    }
-  }
+  fuzzBizzNezz(fuzz, bizz, nezz);
+
   
   return 0;
 }
